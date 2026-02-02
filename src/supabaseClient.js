dea14552f0bf41a8b35a54950c2c3190934c798e
Zainab Fatema <zainabfadema537@gmail.com>
2026-02-02 14:24:26 +0530

    removed env from tracking

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Ye dono cheezein aapko Supabase Dashboard > Settings > API mein milengi
-const supabaseUrl = 'https://eylbajdshesiqvalwphl.supabase.co' 
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV5bGJhamRzaGVzaXF2YWx3cGhsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk3NTExNjIsImV4cCI6MjA4NTMyNzE2Mn0.w1hBF1W0ajevcWlAIo0CPK3A3SXSOPLJbKZJJW3TN9g' 
+const supabaseUrl = process.env.ProjectURL 
+const supabaseAnonKey = process.env.AnonKey
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
